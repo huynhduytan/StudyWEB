e395b9a93fe37a09545e208c2535f80fc2500dab
huynhduytan <huynhduytan0501@gmail.com>
2019-08-01 19:25:21 +0700

    cap nhật

--- loaisanpham/danhsach.php
+++ loaisanpham/danhsach.php
@@ -12,10 +12,35 @@ while ($row = mysqli_fetch_array($result, MYSQLI_ASSOC)) {
 }
 /*
 [
-    ['lsp_ma' => 1, 'lsp_ten' => 'MÃ¡y tÃ­nh báº£ng', 'lsp_mota' => ''],
-    ['lsp_ma' => 2, 'lsp_ten' => 'MÃ¡y tÃ­nh báº£ng', 'lsp_mota' => ''],
-    ['lsp_ma' => 3, 'lsp_ten' => 'MÃ¡y tÃ­nh báº£ng', 'lsp_mota' => ''],
+    ['lsp_ma' => 1, 'lsp_ten' => 'MÃ¡y tÃ­nh báº£ng', 'lsp_mota' => ''], row1
+    ['lsp_ma' => 2, 'lsp_ten' => 'MÃ¡y tÃ­nh báº£ng', 'lsp_mota' => ''], row2
+    ['lsp_ma' => 3, 'lsp_ten' => 'MÃ¡y tÃ­nh báº£ng', 'lsp_mota' => ''], row3
+    ...
 ]
 */
-print_r($data);die;
-?> 
+// print_r($data);die;
+// var_dump($data);die;
+?>
+
+<table border="1">
+    <tr>
+        <th>Mã</th>
+        <th>Tên</th>
+        <th>Mô tả</th>
+        <th>Chức năng</th>
+    </tr>
+    <?php 
+    $biendem=1;
+    ?>
+    <?php foreach($data as $row): ?>
+    <tr>
+        <td><?= $row['lsp_ma']; ?></td>
+        <td><?= $row['lsp_ten']; ?></td>
+        <td><?php echo $row['lsp_mota']; ?></td>
+        <td>
+            <a href="/studyWED/sua.php">Sửa</a>
+        </td>
+    </tr>
+    <?php endforeach; ?>
+</table>
+© 2019 GitHub, Inc.
---
+++ loaisanpham/sua.php
@@ -0,0 +1,1 @@
+<h1>Trang sửa sản phẩm</h1>
