af0c29510a5882c51bf4814a776dfda5b73fe382
huynhduytan <huynhduytan0501@gmail.com>
2019-08-22 18:37:53 +0700

    bo sung

--- hinhsanpham/them.php
+++ hinhsanpham/them.php
@@ -33,4 +33,44 @@ while ($row = mysqli_fetch_array($resultSanPham, MYSQLI_ASSOC)) {
         <i class="fa fa-heartbeat" aria-hidden="true"></i> Lưu
     </button>
 </form>
+<?php
+if(isset($_POST['btnLuu'])) {
+    // Đường dẫn để chứa thư mục upload trên ứng dụng web của chúng ta. Các bạn có thể tùy chỉnh theo ý các bạn.
+    // Ví dụ: các file upload sẽ được lưu vào thư mục ./../public/uploads
+    $upload_dir = "./../public/uploads/";
+    // Đối với mỗi file, sẽ có các thuộc tính như sau:
+    
+    // $_FILES['hsp_tentaptin']['name'] = '4707_sao-khuya.jpg';
+    // $_FILES['hsp_tentaptin']['type'] = '.jpg';
+    // $_FILES['hsp_tentaptin']['tmp_name'] = 'AABB1123.tmp';
+    // $_FILES['hsp_tentaptin']['error'] = '0';
+    // $_FILES['hsp_tentaptin']['size'] = '35Kb';
+    // $_FILES['hsp_tentaptin']['name']     : Tên của file chúng ta upload
+    // $_FILES['hsp_tentaptin']['type']     : Kiểu file mà chúng ta upload (hình ảnh, word, excel, pdf, txt, ...)
+    // $_FILES['hsp_tentaptin']['tmp_name'] : Đường dẫn đến file tạm trên web server
+    // $_FILES['hsp_tentaptin']['error']    : Trạng thái của file chúng ta upload, 0 => không có lỗi
+    // $_FILES['hsp_tentaptin']['size']     : Kích thước của file chúng ta upload
+    // Nếu file upload bị lỗi, tức là thuộc tính error > 0
+    // if($_FILES['hsp_tentaptin']['size'] > 3500000000) {
+    //     echo 'vuot qua/...';die;
+    // }
+    // if($_FILES['hsp_tentaptin']['type'] == '.php'
+    //     || $_FILES['hsp_tentaptin']['type'] == '.exe'
+    //     $_FILES['hsp_tentaptin']['type'] == '.cs'
+    // ) {
+    //     echo 'File k dc phep upload';die;
+    // }
+    if ($_FILES['hsp_tentaptin']['error'] > 0)
+    {
+        echo 'File Upload Bị Lỗi';die;
+    }
+    else{
+        // Tiến hành di chuyển file từ thư mục tạm trên server vào thư mục chúng ta muốn chứa các file uploads
+        // Ví dụ: move file từ C:\xampp\tmp\php6091.tmp -> C:/xampp/htdocs/learning.nentang.vn/php/twig/assets/uploads/hoahong.jpg
+        $hsp_tentaptin = $_FILES['hsp_tentaptin']['name'];
+        move_uploaded_file($_FILES['hsp_tentaptin']['tmp_name'], $upload_dir.$hsp_tentaptin);
+        echo 'File Uploaded';
+    }
+}
+?>
 
