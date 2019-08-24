590c8f8081588aff73a4beb5c2b34cb8cfb1137c
huynhduytan <huynhduytan0501@gmail.com>
2019-08-24 17:44:13 +0700

    update 17:43

--- loaisanpham/xoa.php
+++ loaisanpham/xoa.php
@@ -1,13 +1,7 @@
-<form id="xoasanpham" name="xoasanpham" method="post" action="/web/xoa.php">
-    ID Cần Xóa: <input type="text" name="xsp" id="xsp"></br>
-    <input type="submit" name="Xoa" value="Xóa">
-</form>
 <?php
-    if(isset($_POST['Xoa'])){
-        $conn= mysqli_connect('127.0.0.1', 'root', '', 'salomon') or die ('không kết nối');
-        $id=$_POST['xsp'];
-        $sql="DELETE FROM loaisanpham  WHERE lsp_ma=$id";
-        mysqli_query($conn,$sql);
-        echo 'Thanh Cong';
-    }
+    require_once __DIR__ . '/../dbconnect.php';
+    $lsp_ma = $_GET['id'];
+    $sqlDelete = "DELETE FROM loaisanpham WHERE lsp_ma = '$lsp_ma';";
+    $result = mysqli_query($conn,$sqlDelete);
+    header("location:capnhatdanhsach.php");
 ?>
